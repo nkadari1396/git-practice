566b12e1eeaf475a8d800900197d6696bbdf7447
System Administrator <root@Naveens-MacBook-Pro.local>
2026-03-07 13:57:39 +0800

    Variables

--- 04-variables-arguments.sh
+++ 04-variables-arguments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=Naveen
-PERSON2=Raju
+PERSON1=$1
+PERSON2=$2
 
 echo "$PERSON1 :: Hi ${PERSON2}, How are you?"
 echo "$PERSON2 :: Hi ${PERSON1}, I am doing well, what about you?"
---
+++ 05-variables-sensitive.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Please enter your username::"
+
+read -s USERNAME
+echo "username you entered is: $USERNAME"
+
+echo "Please enter your password::"
+
+read -s PASSWORD
+
+echo "password you enterd is $PASSWORD
