87f6e2481dfc267bd9e0001535034a9377397247
System Administrator <root@Naveens-MacBook-Pro.local>
2026-03-07 13:29:03 +0800

    Converstions

---
+++ 02-conersation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh :: Hi Suresh, How are you?"
+echo "Suresh :: Hi Ramesh, I am good, How are you doing?"
+echo "Ramesh :: I am good Suresh, Whats going on?"
+echo "Suresh :: I am learning shell scripting Ramesh"
