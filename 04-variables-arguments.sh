b1add28f58c0272b5c2c9c639847fb1781d211c8
System Administrator <root@Naveens-MacBook-Pro.local>
2026-03-07 13:59:45 +0800

    Variables

--- 04-variables-arguments.sh
+++ 04-variables-arguments.sh
@@ -6,4 +6,4 @@ PERSON2=$2
 echo "$PERSON1 :: Hi ${PERSON2}, How are you?"
 echo "$PERSON2 :: Hi ${PERSON1}, I am doing well, what about you?"
 echo "$PERSON1 :: I am doing good ${PERSON2}, Whats going on?"
-echo "$PERSON2 :: I am learning shell
+echo "$PERSON2 :: I am learning shell scripting ${PERSON1}"
