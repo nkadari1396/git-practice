5077da50770adece70c14d146ea33cf0d9e3459c
System Administrator <root@Naveens-MacBook-Pro.local>
2026-03-07 13:44:42 +0800

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Naveen
+PERSON2=Raju
+
+echo "$PERSON1 :: Hi ${PERSON2}, How are you?"
+echo "$PERSON2 :: Hi ${PERSON1}, I am doing well, what about you?"
+echo "$PERSON1 :: I am doing good ${PERSON2}, Whats going on?"
+echo "$PERSON2 :: I am learning shell scripting ${PERSON1}"
